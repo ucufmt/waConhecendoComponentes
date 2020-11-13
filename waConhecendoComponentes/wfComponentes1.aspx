def5fd25b9a64821e0ebb180c204a057b8cb2b31
ucufmt <ufmtconvidativa@gmail.com>
2020-11-13 01:39:33 -0400

    Adicionar arquivos de projeto.

---
+++ waConhecendoComponentes/wfComponentes1.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="wfComponentes1.aspx.cs" Inherits="waConhecendoComponentes.wfComponentes1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head runat="server">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <title>Componentes</title>
+        <style type="text/css">
+            .auto-style1 {
+                width: 300px;
+            }
+            .auto-style2 {
+                width: 442px;
+            }
+        </style>
+    </head>
+    <body>
+        <form id="form1" runat="server">
+            <div>
+                <table style="width:100%;">
+                    <tr>
+                        <td class="auto-style1">Site</td>
+                        <td class="auto-style2">Endereço</td>
+                        <td>Opções</td>
+                    </tr>
+                    <tr>
+                        <td class="auto-style1">
+                            <asp:TextBox ID="txtSite" runat="server" Width="260px"></asp:TextBox>
+                        </td>
+                        <td class="auto-style2">
+                            <asp:TextBox ID="txtEndereco" runat="server" Width="403px"></asp:TextBox>
+                        </td>
+                        <td>
+                            <asp:Button ID="btInserir" runat="server" Text="Button" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="auto-style1">
+                            <asp:DropDownList ID="DropDownList1" runat="server" DataSourceID="SqlDataSource1" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                            </asp:DropDownList>
+                            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:univercidadecoConnectionString %>" ProviderName="<%$ ConnectionStrings:univercidadecoConnectionString.ProviderName %>" SelectCommand="SELECT * FROM [chamada]"></asp:SqlDataSource>
+                        </td>
+                        <td class="auto-style2">&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td class="auto-style1">&nbsp;</td>
+                        <td class="auto-style2">&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                </table>
+            </div>
+        </form>
+    </body>
+</html>
---
+++ waConhecendoComponentes/wfComponentes1.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace waConhecendoComponentes
+{
+
+
+    public partial class wfComponentes1
+    {
+
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Controle txtSite.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtSite;
+
+        /// <summary>
+        /// Controle txtEndereco.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEndereco;
+
+        /// <summary>
+        /// Controle btInserir.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btInserir;
+
+        /// <summary>
+        /// Controle DropDownList1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+
+        /// <summary>
+        /// Controle SqlDataSource1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+    }
+}
